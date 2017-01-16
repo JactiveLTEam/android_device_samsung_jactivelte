d9088227244ae3177d14aaca679f31f94922e2f0
Vlad <tsvetkov_vlad@ukr.net>
2017-01-16 14:21:30 +0200

    changed build fingerprint

--- lineage.mk
+++ lineage.mk
@@ -6,8 +6,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jactivelte \
     TARGET_DEVICE=jactivelte \
-    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:4.4.2/KTU84Q/I9295XXUCNE5:user/release-keys" \
-    PRIVATE_BUILD_DESC="jactivelte-user 4.4.2 KTU84Q I9295XXUCNE5 release-keys"
+    BUILD_FINGERPRINT="samsung/jactivelte/jactivelte:5.0.1/LRX22C/I9295XXUDPF1:user/release-keys" \
+    PRIVATE_BUILD_DESC="jactivelte-user 5.0.1 LRX22C I9295XXUDPF1 release-keys"
 
 PRODUCT_DEVICE := jactivelte
 PRODUCT_NAME := lineage_jactivelte
